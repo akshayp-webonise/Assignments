948877a7e882e749cadc462a9f283bdf26b8a3b5
unknown <akshay.pawar@weboniselab.com>
2016-12-23 20:08:21 +0530

    Drop Down Click and Hover

--- DropDown/js/dropdown.js
+++ DropDown/js/dropdown.js
@@ -1,34 +1,62 @@
 $(document).ready(function() {
 	$(".innerList").hide();
-
+	$(".subMenu").hide();
 	// Hover Code
 
-	// $(".headerLinks>li").hover(function() {
-	// 	$(this).children('.innerList').stop().slideToggle();
-	// });
+	$(".headerLinks>li").hover(function() {
+		$(this).children('.innerList').stop().slideToggle();
+	});
+
+	$(".innerList li").hover(function() {
+		$(this).children('ul').stop().slideToggle();
+	});
+
+
 
 	// Click Code
 
-	$(".headerLinks a").click(function() {
-		var	getID = $(this).attr('href');
-		$(".innerList").hide();		
-		$(getID).parent().siblings().removeClass('active');
-		$(getID).parent().toggleClass('active');
-		if($(this).parent().attr("class")==('active')) {
-			$(getID).show();
-		} else {
-			$(getID).hide();	
-		}
-	});
+	// $(".headerLinks>li>a").click(function() {
+	// 	var	getID = $(this).attr('href');
+	// 	$(".innerList").hide();		
+	// 	$(getID).parent().siblings().removeClass('active');
+	// 	$(getID).parent().toggleClass('active');
+	// 	if($(this).parent().attr("class")==('active')) {
+	// 		$(getID).show();
+	// 	} else {
+	// 		$(getID).hide();
+	// 	}
+	// });
 
-	$(document).mouseup(function (e) {
-	    var container = $(".innerList");
+	// $(".innerList>li>a").click(function() {
+	// 	var	getIDTwo = $(this).attr('href');
+	// 	$(".subMenu").hide();		
+	// 	$(getIDTwo).parent().siblings().removeClass('active');
+	// 	$(getIDTwo).parent().toggleClass('active');
+	// 	if($(this).parent().attr("class")==('active')) {
+	// 		$(getIDTwo).show();
+	// 	} else {
+	// 		$(getIDTwo).hide();
+	// 	}
+	// });
 
-	    if (!container.is(e.target) 
-	        && container.has(e.target).length === 0)
-	    {
-	        container.hide();
-	    }
-	});
+	// $(document).mouseup(function (e) {
+	//     var container = $(".innerList");
+
+	//     if (!container.is(e.target) 
+	//         && container.has(e.target).length === 0)
+	//     {
+	//         container.hide();
+	//         container.parent().removeClass('active');
+	//     }
+
+	//     var containerTwo = $(".subMenu");
+
+	//     if (!containerTwo.is(e.target) 
+	//         && containerTwo.has(e.target).length === 0)
+	//     {
+	//         containerTwo.hide();
+	//         containerTwo.parent().removeClass('active');
+	//     }
+	// });
 
 });
